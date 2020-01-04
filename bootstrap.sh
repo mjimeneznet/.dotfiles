dc58efe2717eec3d1472167bc7032e78e4486c81
Manuel Jimenez <mjimenez@mjimenez.net>
2020-01-04 23:40:14 +0100

    Added ranger and minimal configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,21 @@
 if [[ "$OSTYPE" == *"linux"* ]]; then
 	if [[ -f /etc/arch-release ]]; then
 		echo "I'm $(cat /etc/arch-release)" 
-		echo "Installing packages..."
-		sudo pacman -Syuq --noconfirm git zsh
+		echo -e "Installing packages..." 
+		echo "git (distributed control version system)" 
+		echo "zsh (zsh shell)"
+		echo "kitty (terminal emulator with graphics support and ligatures)" 
+		echo "exa (ls replacement)" 
+		echo "tldr (man lightweight)" 
+		echo "diff-so-fancy (diff replacement)"
+		echo "bat (cat replacement)"
+		echo "ripgrep (grep replacement)" 
+		echo "fzf (fuzzy finder)"
+		echo "fd (find alternative)" 
+		echo "fasd (command-line productiviy booster)"
+		echo "emojify (emojis!)"
+		echo "ranger (console file manager with graphics support)"
+		sudo pacman -Syu --noconfirm git zsh kitty exa tldr diff-so-fancy bat ripgrep fzf fd fasd emojify ranger ffmpegthumbnailer 
 		echo "Configuring git..."
 		git config --global user.name "Manuel Jimenez"
 		git config --global user.email "mjimenez@mjimenez.net"
@@ -23,26 +36,6 @@ if [[ "$OSTYPE" == *"linux"* ]]; then
 			echo "Installing extra fonts..."
 			git clone https://github.com/ryanoasis/nerd-fonts.git /tmp/nerd-fonts && bash /tmp/nerd-fonts/install.sh -A	
 		fi
-		echo "Installing Kitty terminal..."
-		sudo pacman -Syuq --noconfirm kitty
-		echo "Installing exa (ls replacement)..."
-		sudo pacman -Syuq --noconfirm exa
-		echo "Installing TL;DR (man lightweight)..."
-		sudo pacman -Syu --noconfirm tldr
-		echo "Installing diff-so-fancy (diff replacement)..."
-		sudo pacman -Syu --noconfirm diff-so-fancy
-		echo "Installing bat (cat replacement)..."
-		sudo pacman -Syu --noconfirm bat
-		echo "Installing ripgrep (grep replacement)..."
-		sudo pacman -Syu --noconfirm ripgrep
-		echo "Installing fzf (fuzzy finder)..."
-		sudo pacman -Syu --noconfirm fzf
-		echo "Installing fd (find alternative)..."
-		sudo pacman -Syu --noconfirm fd
-		echo "Installing Fasd (command-line productivity booster)..."
-		sudo pacman -Syu --noconfirm fasd
-		echo "Installing emojify (because I'm millenial)..."
-		sudo pacman -Syu --noconfirm emojify
 	fi
 fi
 
