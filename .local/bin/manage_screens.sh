3eb0fb41a2dd59aefe5991c82c8f80871eda443f
Manuel Jimenez <m.jimenez@stuart.com>
2020-06-05 10:41:05 +0200

    Adding more logic for the laptop

--- .local/bin/manage_screens.sh
+++ .local/bin/manage_screens.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+LAPTOP=$(cat /sys/class/drm/card0-eDP-1/status)
+EXTERNAL=$(cat /sys/class/drm/card0-DP-2/status)
+
 if [[ ! $(hostname) == "bender" ]]; then
-	xrandr --output DP-2 --auto --primary
-	xrandr --output eDP-1 --off
+	if [[ $LAPTOP == "connected" ]] && [[ $EXTERNAL == "disconnected" ]]; then
+		return 0
+	else
+		xrandr --output DP-2 --auto --primary
+		xrandr --output eDP-1 --off
+	fi
 else
-	echo "Nothing"
+	return 0	
 fi
 
